e72376ca14e0d6490cce5456381b4e2ae7dd4bda
Jack Adamczyk <prepinavest@gmail.com>
2024-04-23 11:50:44 -0400

    got modal working

--- scripts/index.js
+++ scripts/index.js
@@ -28,11 +28,31 @@ const initialCards = [
 const profileEditBtn = document.getElementById("profile-edit-button");
 const profileExitBtn = document.getElementById("profile-exit-button");
 const profileEditModal = document.getElementById("profile-edit-motal");
+const profileTitle = document.getElementById("profile-title");
+const profileDescription = document.getElementById("profile-desc");
+const profileTitleInput = document.getElementById("profile-title-input");
+const profileDescriptionInput = document.getElementById(
+  "profile-description-input"
+);
+const profileEditForm = profileEditModal.querySelector(".modal__form");
+
+function closePopup() {
+  profileEditModal.classList.remove("modal__opened");
+}
 
 profileEditBtn.addEventListener("click", () => {
+  profileTitleInput.value = profileTitle.textContent;
+  profileDescriptionInput.value = profileDescription.textContent;
   profileEditModal.classList.add("modal__opened");
 });
 
 profileExitBtn.addEventListener("click", () => {
-  profileEditModal.classList.remove("modal__opened");
+  closePopup();
+});
+
+profileEditForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  profileTitle.textContent = profileTitleInput.value;
+  profileDescription.textContent = profileDescriptionInput.value;
+  closePopup();
 });
